a8100cb26233211b9b6a1e39ce037f786ac940ae
magic62442 <whuliqiyan@gmail.com>
2019-12-05 21:39:07 +0800

    try handle conflict

--- api/urls.py
+++ api/urls.py
@@ -6,11 +6,8 @@ router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 router.register(r'stations', views.StationViewSet)
-<<<<<<< HEAD
-=======
 router.register(r'lines', views.LineViewSet)
 router.register(r'routes', views.RouteViewSet)
->>>>>>> 94c587dc87862e0bf19664e34b0024ddfab731df
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
